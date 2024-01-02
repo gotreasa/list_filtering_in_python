39bec71d9fe81b6e71411862b08310d8461756ef
Gearoid O'Treasaigh <gotreasa@users.noreply.github.com>
2024-01-02 17:37:12 +0000

    refactor: make use of a filter

--- modules/list_filtering.py
+++ modules/list_filtering.py
@@ -1,8 +1,11 @@
+def is_integer(item) -> bool:
+    return isinstance(item, int)
+
+
 def filter_list(items: list) -> list:
     if not isinstance(items, list):
         raise ValueError("❗️ Input should be a list")
     result = []
-    for item in items:
-        if isinstance(item, int):
-            result.append(item)
+    for item in filter(is_integer, items):
+        result.append(item)
     return result
